9b8c9b299f01aa7c9d01c4d7f4f70e4778ab7c90
Clint Yarwood <chanandler@me.com>
2025-10-16 15:56:19 +0100

    Initial Commit

---
+++ Server Checker/Server_CheckerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Server_CheckerApp.swift
+//  Server Checker
+//
+//  Created by Clint Yarwood on 16/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct Server_CheckerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
